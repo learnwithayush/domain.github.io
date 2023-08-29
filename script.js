7e705cd2ab0e75462bf265ea5b45d110cb0aaf15
learnwithayush <domain.ayush@gmail.com>
2023-08-29 22:21:04 +0530

    change#17 index.html script js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-var clipboard = new ClipboardJS('.copy-button');
+var copyButtons = document.querySelectorAll('.copy-button');
 
-clipboard.on('success', function(e) {
-    e.clearSelection();
-    e.trigger.textContent = 'Copied!';
-    setTimeout(function() {
-        e.trigger.textContent = 'Copy';
-    }, 2000);
+copyButtons.forEach(function(button) {
+    var command = button.previousElementSibling.querySelector('code').textContent;
+
+    new ClipboardJS(button, {
+        text: function() {
+            return command;
+        }
+    });
+
+    button.addEventListener('click', function() {
+        button.textContent = 'Copied!';
+        setTimeout(function() {
+            button.textContent = 'Copy';
+        }, 2000);
+    });
 });
