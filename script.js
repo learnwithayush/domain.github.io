33860cc5053b08aef4f2dc7f334cba410f2a9b2b
learnwithayush <143501400+learnwithayush@users.noreply.github.com>
2023-11-12 01:32:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("imageInput").addEventListener("change", handleImage);
 });
 
@@ -9,7 +9,7 @@ function handleImage() {
     if (image) {
         const reader = new FileReader();
 
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             const img = document.getElementById("resizedImage");
             img.src = e.target.result;
             img.style.display = "block";
@@ -31,24 +31,28 @@ function resizeImage() {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
 
-    canvas.width = pixelSize;
-    canvas.height = pixelSize;
-
     const image = new Image();
     image.src = resizedImage.src;
 
-    image.onload = function() {
-        ctx.drawImage(image, 0, 0, pixelSize, pixelSize);
+    image.onload = function () {
+        const aspectRatio = image.width / image.height;
+        const newWidth = pixelSize;
+        const newHeight = pixelSize / aspectRatio;
 
-        canvas.toBlob(function(blob) {
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+
+        ctx.drawImage(image, 0, 0, newWidth, newHeight);
+
+        canvas.toBlob(function (blob) {
             const url = URL.createObjectURL(blob);
             downloadLink.href = url;
             downloadLink.style.display = "block";
         }, "image/jpeg", 0.7); // 0.7 is the quality of the image
     };
 
-    downloadLink.addEventListener("click", function() {
-        setTimeout(function() {
+    downloadLink.addEventListener("click", function () {
+        setTimeout(function () {
             URL.revokeObjectURL(downloadLink.href);
         }, 1500);
     });
