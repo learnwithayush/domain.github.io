7d1c281a16d81927916768cbe73911192a7112f3
ayush <ayush@infoedge.com>
2024-02-01 09:38:27 +0530

    1st commit for mol

---
+++ ecom/script.js
@@ -0,0 +1,9 @@
+function changeColor(color, productId) {
+    document.getElementById(productId).style.backgroundColor = color;
+}
+
+function submitForm() {
+    // You can add the logic to handle form submission (e.g., send data to server) here
+    alert("Form submitted successfully!");
+}
+
